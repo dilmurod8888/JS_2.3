8096d3489d03a1b70f0e218e1a8885836aebec36
Dostonbek <dostonbeksodikov8@gamil.com>
2024-05-28 16:18:15 +0500

    dilmurod

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var age =+prompt("Yoshingizni kiriting")
+var son_1 = +prompt("1 - son kiriting: ");
+var son_2 = +prompt("2 - son kiriting: ");
+var son_3 = +prompt("3 - son kiriting: ");
 
-if (age <= 18 && age>= 0){
-    alert("Yoshsiz,  O'qishingiz kerak")
-}else if(age <= 50 && age > 18){
-    alert("Islashingiz kerak")
-}else if(age <= 59 && age > 50){
-    alert("Yaqinda pensiyaga chiqasiz")
-}else if(age <= 150 && age > 59){
-    alert("Pensiyanerga o'xshaysiz, Hali hayot bo'lsangiz:")
+if (son_1 < son_2 && son_2 < son_3 || son_1 > son_2 && son_2 > son_3){
+    alert("O'rtacha qiymat = "+son_2)
+}
+else if(son_2 > son_1 && son_1 > son_3 || son_2 < son_1 && son_1 < son_3){
+    alert("O'rtacha qiymat = "+son_1)
+}
+else if(son_1 > son_3 && son_3 > son_2 || son_2 < son_3 && son_3< son_1){
+    alert("O'rtacha qiymat = "+son_3)
 }else{
-    alert("Nimadir noto'gri ketib qoldi")
+    alert("Bu joy faqat sonlar uchun ajratilgan !!!")
 }
